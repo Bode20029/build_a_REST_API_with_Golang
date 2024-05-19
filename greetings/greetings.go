2cd7f46fe4bdb4649f27f7f786514d6f1d985735
Bode20029 <jry.bode@gmail.com>
2024-05-19 22:44:19 +0700

    Sixth

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,51 +1,26 @@
 package greetings
 
 import (
-	"errors"
-	"fmt"
-	"math/rand"
+	"regexp"
+	"testing"
 )
 
-// Hello returns a greeting for the named person.
-func Hello(name string) (string, error) {
-	// If no name was given, return an error with a message.
-	if name == "" {
-		return name, errors.New("empty name")
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Gladys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	}
-	// Create a message using a random format.
-	message := fmt.Sprintf(randomFormat(), name)
-	return message, nil
 }
 
-// Hellos returns a map that associates each of the named people
-// with a greeting message.
-func Hellos(names []string) (map[string]string, error) {
-	// A map to associate names with messages.
-	messages := make(map[string]string)
-	// Loop through the received slice of names, calling
-	// the Hello function to get a message for each name.
-	for _, name := range names {
-		message, err := Hello(name)
-		if err != nil {
-			return nil, err
-		}
-		// In the map, associate the retrieved message with
-		// the name.
-		messages[name] = message
+// TestHelloEmpty calls greetings.Hello with an empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
 	}
-	return messages, nil
-}
-
-// randomFormat returns one of a set of greeting messages. The returned
-// message is selected at random.
-func randomFormat() string {
-	// A slice of message formats.
-	formats := []string{
-		"Hi, %v. Welcome!",
-		"Great to see you, %v!",
-		"Hail, %v! Well met!",
-	}
-
-	// Return one of the message formats selected at random.
-	return formats[rand.Intn(len(formats))]
 }
